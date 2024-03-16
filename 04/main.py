804bc5cfa36f79ebc551bcfc94ce7a01701a93b1
Cristhian <cristhianclx@gmail.com>
2024-03-15 20:47:45 -0500

    update 04

--- 04/main.py
+++ 04/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.sql import func
@@ -16,7 +16,8 @@ migrate = Migrate(app, db)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
+    first_name = db.Column(db.String(200))
+    last_name = db.Column(db.String(200))
     age = db.Column(db.Integer, nullable=False)
     content = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
@@ -26,3 +27,19 @@ class User(db.Model):
 def users():
     users_data = User.query.all()
     return render_template("users.html", users=users_data)
+
+
+@app.route("/users/<int:id>")
+def users_by_id(id):
+    user = User.query.get_or_404(id)
+    return render_template("user.html", user=user)
+
+
+@app.route("/users/add", methods=["GET", "POST"])
+def users_add():
+    if request.method == "GET":
+        return render_template("users-add.html")
+    if request.method == "POST":
+        import ipdb; ipdb.set_trace()
+        
+    
