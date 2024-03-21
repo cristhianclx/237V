d36bdfb579f72914979a5b7768eb52b4b2599db9
Cristhian <cristhianclx@gmail.com>
2024-03-20 22:30:17 -0500

    09

---
+++ 09/main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Cibertec2024'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.debug = True
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+ma = Marshmallow(app)
+
+socketio = SocketIO(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(150), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return "<Message {}>".format(self.id)
+
+
+class MessageSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "message", "created_at")
+        model = Message
+        datetimeformat="%Y-%m-%d %H:%M"
+
+
+messages_schema = MessageSchema(many = True)
+
+
+@app.route("/")
+def home():
+    messages_data = Message.query.all()
+    messages_results = messages_schema.dump(messages_data)
+    return render_template("home.html", messages=messages_results)
+
+
+@socketio.on("welcome")
+def welcome(data, methods=["GET", "POST"]):
+    print("on welcome, data: {}".format(data))
+
+
+def messagesReceived(methods=["GET", "POST"]):
+    print("message was received")
+
+
+@socketio.on("messages")
+def messages(data, methods=["GET", "POST"]):
+    # {'username': 'cristhian', 'message': '123456'}
+    message = Message(**data)
+    db.session.add(message)
+    db.session.commit()
+    socketio.emit("messages-response", data, callback=messagesReceived)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
